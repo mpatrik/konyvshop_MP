9087d021e41fadfc6d1f602dfd3fe9a880028d6e
Menyhárt Patrik <menyhartpatrik05@gmail.com>
2022-04-11 15:35:05 +0200

    create main, products pages

--- konyvshop/src/app/app.module.ts
+++ konyvshop/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { ProductsComponent } from './pages/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
