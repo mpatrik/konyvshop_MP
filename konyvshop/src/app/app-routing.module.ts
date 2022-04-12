21ceb5363a031d172e3f8c126349f71da6d45017
Menyhárt Patrik <menyhartpatrik05@gmail.com>
2022-04-12 14:17:40 +0200

    create, style menu

--- konyvshop/src/app/app-routing.module.ts
+++ konyvshop/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
-import { NgModule } from '@angular/core';
+import {Component, NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CartComponent} from "./pages/cart/cart.component";
+import {LoginComponent} from "./pages/login/login.component";
+import {SignupComponent} from "./pages/signup/signup.component";
+import {MainComponent} from "./pages/main/main.component";
+import {ProductsComponent} from "./pages/products/products.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'main', component: MainComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: '**', component: MainComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- konyvshop/src/app/app.module.ts
+++ konyvshop/src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './pages/main/main.component';
 import { ProductsComponent } from './pages/products/products.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    ProductsComponent
+    ProductsComponent,
+    CartComponent,
+    LoginComponent,
+    SignupComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
