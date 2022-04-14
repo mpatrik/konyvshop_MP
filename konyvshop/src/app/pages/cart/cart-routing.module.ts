b3082d50d9ff9ea977c5ee050aaef9c316d303da
Menyhárt Patrik <menyhartpatrik05@gmail.com>
2022-04-14 19:40:37 +0200

    products, routing upgrade (cart fix)

--- konyvshop/src/app/pages/cart/cart-routing.module.ts
+++ konyvshop/src/app/pages/cart/cart-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {MainComponent} from "../main/main.component";
+import {CartComponent} from "./cart.component";
 
-const routes: Routes = [{path: '', component: MainComponent}];
+const routes: Routes = [{path: '', component: CartComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
