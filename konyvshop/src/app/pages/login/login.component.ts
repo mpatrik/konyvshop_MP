c44eef6f56bb91191cae4f464c9c387481a2ace5
Menyhárt Patrik <menyhartpatrik05@gmail.com>
2022-04-15 21:43:14 +0200

    login observable

--- konyvshop/src/app/pages/login/login.component.ts
+++ konyvshop/src/app/pages/login/login.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl} from "@angular/forms";
 import {Router} from "@angular/router";
 import {FakeLoadingService} from "../../shared/services/fake-loading.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
   email = new FormControl('');
   password = new FormControl('');
+
+  loadingSubscription?: Subscription;
 
   constructor(private router: Router, private loadingService: FakeLoadingService) { }
 
@@ -39,14 +42,14 @@ export class LoginComponent implements OnInit {
     */
 
     // Observable
-    this.loadingService.loadingWithObservable(this.email.value, this.password.value).subscribe((data: number) => {
+    const subscription = this.loadingService.loadingWithObservable(this.email.value, this.password.value).subscribe((data: boolean) => {
       console.log(data);
-    })
-
+    });
   }
 
-
-
+  ngOnDestroy() {
+    this.loadingSubscription?.unsubscribe();
+  }
 
 
 }
